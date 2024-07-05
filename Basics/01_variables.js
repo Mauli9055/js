2763f551f3c9bb167ba05ee567791b0a3646e3b2
Mauli9055 <134618752+Mauli9055@users.noreply.github.com>
2024-07-05 10:22:49 +0000

    datatypes

---
+++ Basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 9055
+let accountEmail = "ashok@gmail.com"
+var accountPassword = "12345"
+accountcity = "Ahmednagar"
+let accountstate;
+
+accoundId = 2
+
+accountEmail = "ashok1234@.com"
+accountPassword = "54321"
+accountcity = "Pune"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountcity, accountstate])
+/*
+prefer not to use var
+bacoz of issue in blockscope and functional scope
+
+*/
---
+++ Basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict"; // treat all JS code as newer version
+
+let name ="Ashok";
+let age = 29;
+let isloggedIn = false;
+
+//number
+// bigin
+// string 
+//boolean
+// null => stand alone
+// undefined => 
+// symbole => unqieness
+
+//object
+
+console.log(typeof age);
+console.log(typeof null); // null is a object in typeof 
+console.log(typeof undefined);
